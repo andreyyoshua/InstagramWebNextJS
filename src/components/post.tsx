819a1a64fb6300eb47e9042945aa88a240808f41
Andrey Yoshua Manik <andrey.yoshua@gmail.com>
2024-04-05 07:54:39 +0700

    add comments and input box

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,4 +1,4 @@
-import { BanknotesIcon, TvIcon, CogIcon, EyeIcon } from '@heroicons/react/20/solid'
+import { BanknotesIcon, TvIcon, CogIcon, EyeIcon, KeyIcon } from '@heroicons/react/20/solid'
 export interface PostData {
     id: string,
     username: string,
@@ -11,21 +11,34 @@ export interface PostData {
 export default function Post({post}: {post: PostData}) {
     return (
         <div className='bg-white my-7 border rounded-md'>
+            {/* Post Headers */}
             <div className="flex items-center p-5">
                 <img src={post.userImg} alt="user-img" className="h-12 rounded-full object-cover border p-1 mr-3" />
                 <p className="font-bold flex-1">{post.username}</p>
                 <BanknotesIcon className='h-5'/>
             </div>
+
+            {/* Post Images */}
             <img src={post.img} alt="img-post" className='object-cover w-full' />
             
-            <div className='flex justify-between px-4'>
+            {/* Post Buttons */}
+            <div className='flex justify-between px-4 mt-4'>
                 <div className='flex space-x-4'>
                     <TvIcon className='btn'/>
                     <CogIcon className='btn'/>
                 </div>
                 <EyeIcon className='btn' />
             </div>
-            <p>{post.caption}</p>
+
+            {/* Post Caption */}
+            <p className='p-5 truncate'><span className='font-bold mr-2'>{post.username}</span>{post.caption}</p>
+
+            {/* Post Input Box */}
+            <form action="" className='flex items-center p-4'>
+                <KeyIcon className='h-7'/>
+                <input className='border-none flex-1 focus:ring-0' type='text' placeholder='Enter your comment...'/>
+                <button className='text-blue-400 font-bold'>Post</button>
+            </form>
         </div>
     )
 }
