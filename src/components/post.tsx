92a816ff0dcc34b86f522d707974553a311aec16
Andrey Yoshua Manik <andrey.yoshua@gmail.com>
2024-04-05 07:28:03 +0700

    create post header and img

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,4 +1,4 @@
-'use client'
+import { BanknotesIcon} from '@heroicons/react/20/solid'
 export interface PostData {
     id: string,
     username: string,
@@ -10,9 +10,14 @@ export interface PostData {
 
 export default function Post({post}: {post: PostData}) {
     return (
-        <div>
-            {post.username}
-            <img src={post.img} alt="img-post" />
+        <div className='bg-white my-7 border rounded-md'>
+            <div className="flex items-center p-5">
+                <img src={post.userImg} alt="user-img" className="h-12 rounded-full object-cover border p-1 mr-3" />
+                <p className="font-bold flex-1">{post.username}</p>
+                <BanknotesIcon className='h-5'/>
+            </div>
+            <img src={post.img} alt="img-post" className='object-cover w-full' />
+            <p>{post.caption}</p>
         </div>
     )
 }
